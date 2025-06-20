90ccea7aedb9b8e4849d963ec50068967e71e6d8
Praveen Koujalagi <praveenkoujalagi99@gmail.com>
2025-06-21 01:25:34 +0530

    added some concepts of EJS

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 8080;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get(`/hello`, (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get(`/rolldice`, (req, res) => {
+    let dicevalue = Math.floor(Math.random() * 6) + 1;
+    res.render('rolldice.ejs',{dicevalue});
+});
+
+app.get(`/ig/:username`, (req, res) => {
+    let {username} = req.params;
+    const instaData= require(`./data.json`);
+    const data = instaData[username];
+    if(data) {
+        res.render('instagram.ejs',{data});
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
